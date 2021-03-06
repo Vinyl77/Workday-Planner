a44ecad0132e6f5d6f0d1651c4a86738cd8cf10d
Richard Yvarra <richard.yvarra@gmail.com>
2021-03-05 19:31:57 -0500

    more script additions

--- assets/script.js
+++ assets/script.js
@@ -16,9 +16,28 @@ $(document).ready(function(){
         console.log(text, time);
         // set the items in the local storage
         localStorage.setItem(time, text);
-
     })
- 
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-})
+    function timeUpdater(){
+        //get current date and time
 
+       var currentHr = parseInt(moment().format("HH"));
+        //loop over each time block
+        $(".time-block").each(function(){   //query parent, will give us access to the children
+    // console.log(parseInt($(this).attr("id").split("r")[1]));
+            var hourBlock = parseInt($(this).attr("id").split("r")[1]);
+            console.log("compare this", currentHr, hourBlock);
+            //check IF time block is in past, present, or future
+
+
+            
