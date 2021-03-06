0ad1f752b3431bf9a2611ec9040141935a147559
Richard Yvarra <richard.yvarra@gmail.com>
2021-03-05 19:02:55 -0500

    time appended

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
 
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
+        function update(){
+            $("#currentDay").html(moment().format("MMMM Do YYYY hh:mm:ss a"));
+        }
+        setInterval(update, 1000);
+
